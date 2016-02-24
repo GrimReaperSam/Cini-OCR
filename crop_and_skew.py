5f74ba13972bfc0de700504d97ec91a1085c66f8
GrimReaperSam <fayezlahoud@gmail.com>
2016-02-24 10:20:38 +0100

    Ability to extract image from the scanned page

--- crop_and_skew.py
+++ crop_and_skew.py
@@ -1,5 +1,6 @@
 import cv2
 import utils
+import numpy as np
 
 
 def crop_and_skew(image):
@@ -7,21 +8,22 @@ def crop_and_skew(image):
     orig = image.copy()
     image = cv2.resize(image, (int(image.shape[1]/ratio), 500))
 
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+    gray = cv2.bilateralFilter(image, 11, 75, 75)
+    gray = cv2.cvtColor(gray, cv2.COLOR_BGR2GRAY)
+    # gray = cv2.GaussianBlur(gray, (5, 5), 0)
     edged = cv2.Canny(gray, 75, 200)
 
-    (cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:10]
+    (contours, _) = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    for c in cnts:
-        peri = cv2.arcLength(c, True)
-        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+    max_area = 0
+    box = None
+    for contour in contours:
+        rect = cv2.minAreaRect(contour)
+        (_, (w, h), _) = rect
+        area = w * h
+        if area > max_area:
+            max_area = area
+            box = cv2.cv.BoxPoints(rect)
+    box = np.int0(box)
 
-        if len(approx) == 4:
-            screenCnt = approx
-            break
-
-    cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
-
-    return utils.crop_rectangle_warp(orig, screenCnt.reshape(4, 2) * ratio)
+    return utils.crop_rectangle_warp(orig, box.reshape(4, 2) * ratio)
--- runner.py
+++ runner.py
@@ -1,10 +1,11 @@
 from crop_and_skew import *
+from extract_image import *
 import os
 
 for filename in os.listdir('samples'):
     image = cv2.imread("samples/%s" % filename)
     scan = crop_and_skew(image)
     cv2.imwrite("results/%s-scan.png" % filename, scan)
-    # crop = crop_and_skew(scan, 300)
-    # cv2.imwrite("%s-crop.png" % filename, crop)
+    crop = extract_image(scan)
+    cv2.imwrite("results/%s-crop.png" % filename, crop)
     print "%s has been processed" % filename
