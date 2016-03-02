1cb888f83d25589eaf866db24794515c8560f1fa
GrimReaperSam <fayezlahoud@gmail.com>
2016-03-02 23:24:11 +0100

    Code refactoring and simplify a bit

--- runner.py
+++ runner.py
@@ -1,14 +1,17 @@
 from crop_and_skew import *
 from split_image import *
+import re
 import os
 
-for filename in os.listdir('samples'):
+for filename in sorted(os.listdir('samples')):
+    name = re.sub('_recto.cr2', '', filename)
+    name = re.sub('_', '', name)
     print "Begin processing image %s" % filename
     image = cv2.imread("samples/%s" % filename)
     page = crop_and_skew(image)
-    cv2.imwrite("results/pages/%s-page.png" % filename, page)
+    cv2.imwrite("results/pages/%s-page.png" % name, page)
     crop, text_section = split_image(page)
-    cv2.imwrite("results/images/%s-crop.png" % filename, crop)
-    cv2.imwrite("results/text-sections/%s-text-section.png" % filename, text_section)
+    cv2.imwrite("results/images/%s-crop.png" % name, crop)
+    cv2.imwrite("results/text-sections/%s-text-section.png" % name, text_section)
     print "End processing image %s" % filename
     print
--- split_image.py
+++ split_image.py
@@ -2,6 +2,7 @@ import cv2
 import utils
 import numpy as np
 from itertools import groupby
+from skimage import filters, img_as_ubyte, color
 
 
 # line is of the form (x1, y1, x2, y2)
@@ -10,7 +11,7 @@ def length(line):
     return abs(dx - ox)
 
 
-def y(line):
+def get_y(line):
     return line[1]
 
 
@@ -27,8 +28,9 @@ def split_image(page):
 
     (contours, _) = cv2.findContours(dil.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
+    max_rect = None
+    # max_rect = None
     max_area = 0
-    box = None
     for cnt in contours:
         rect = cv2.minAreaRect(cnt)
         (_, (w, h), _) = rect
@@ -37,14 +39,14 @@ def split_image(page):
             continue
         if area > max_area:
             max_area = area
-            box = cv2.cv.BoxPoints(rect)
-    box = np.int0(box)
+            max_rect = rect
 
-    scan = utils.crop_rectangle_warp(orig, box.reshape(4, 2) * ratio)
+    max_box = np.int0(cv2.cv.BoxPoints(max_rect))
+
+    scan = utils.crop_rectangle_warp(orig, max_box.reshape(4, 2) * ratio)
 
     # Extracting text section
-    gray = cv2.cvtColor(page, cv2.COLOR_BGR2GRAY)
-    edged = cv2.Canny(gray, 15, 200)
+    edged = cv2.Canny(page, 15, 200)
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
     close = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
 
@@ -53,7 +55,7 @@ def split_image(page):
     # Filter out the vertical lines
     h_lines = [line for line in lines if line[1] == line[3]]
     # Filter lines below the image we found above
-    (a, b, _, _) = utils.abcd_rect(box)
+    (a, b, _, _) = utils.abcd_rect(max_box)
     y_min = min(a[1], b[1])
     h_lines = [line for line in h_lines if line[1] < y_min]
     longest = sorted(h_lines, key=length, reverse=True)
@@ -62,13 +64,13 @@ def split_image(page):
     line_groups = groupby(longest, key=length)
     candidates = []
     for (key, data) in line_groups:
-        lowest = max(data, key=y)
+        lowest = max(data, key=get_y)
         candidates.append(lowest)
 
     # Take the longest four horizontal lines
     long_4 = sorted(candidates, key=length, reverse=True)[:4]
 
     # Take the lowest of these lines
-    text_section = orig[0:int(y(max(long_4, key=y)) * ratio), 0:width]
+    text_section = orig[0:int(get_y(max(long_4, key=get_y)) * ratio), 0:width]
 
     return scan, text_section
