e7d66abc7ae10bf639793940dcad378d1bf9ef8a
GrimReaperSam <fayezlahoud@gmail.com>
2016-03-10 14:16:05 +0100

    Adding more tests for barcode

--- tests/decoder_test.py
+++ tests/decoder_test.py
@@ -7,20 +7,14 @@ import barcode
 import json
 from shared import data
 
-SAMPLES_DIR = '../samples'
-
-versos = filter(lambda f: data['VERSO_SUBSTR'] in f, os.listdir("../%s" % data['SAMPLES_DIR']))
-
 
 class DecoderTest(unittest.TestCase):
     def test(self):
         with open("test-results/barcode-results.json") as results:
             expected = json.loads(results.read())
 
-        for filename in sorted(versos):
-            name = re.sub(data['VERSO_SUBSTR'], '', filename)
-            name = re.sub('_', '', name)
-            im = cv2.imread("%s/%s" % (SAMPLES_DIR, filename))
-            crop = crop_and_skew(im)
-            bar_code = barcode.detect(crop)
+        for filename in sorted(os.listdir("../results/pages")):
+            name = re.sub('ve.png', '', filename)
+            im = cv2.imread("../results/pages/%s" % filename)
+            bar_code = barcode.detect(im)
             self.assertEqual(bar_code, expected[name])
