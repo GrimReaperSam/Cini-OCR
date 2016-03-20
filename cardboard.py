3940cba24f32944abab1f2b39d3dafc95777be1a
GrimReaperSam <fayezlahoud@gmail.com>
2016-03-20 09:57:32 +0100

    Improving crop image to better find the region

--- cardboard.py
+++ cardboard.py
@@ -20,23 +20,28 @@ def crop_image_and_text(page):
     orig = page.copy()
     page = cv2.resize(page, (int(width / ratio), 500))
 
-    kernel = np.ones((7, 7), np.uint8)
+    kernel = np.ones((3, 3), np.uint8)
     dilated = cv2.dilate(page, kernel, iterations=1)
+    dilated = cv2.erode(dilated, kernel, iterations=1)
 
     gray = cv2.cvtColor(dilated, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (11, 11), 0)
     grad_x = cv2.Sobel(gray, ddepth=cv2.cv.CV_32F, dx=1, dy=0, ksize=-1)
     grad_y = cv2.Sobel(gray, ddepth=cv2.cv.CV_32F, dx=0, dy=1, ksize=-1)
 
     grad_x = cv2.convertScaleAbs(grad_x)
+    grad_x = cv2.pow(grad_x, 2)
     grad_y = cv2.convertScaleAbs(grad_y)
+    grad_y = cv2.pow(grad_y, 2)
 
     grad = cv2.addWeighted(grad_x, 0.5, grad_y, 0.5, 0)
+    grad = cv2.pow(grad.astype("float32"), 0.5).astype("uint8")
 
-    blur = cv2.GaussianBlur(grad, (5, 5), 0)
-    _, th2 = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    blur = cv2.GaussianBlur(grad, (7, 7), 0)
+    _, thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-    thresh_kernel = np.ones((5, 5), np.uint8)
-    dilated = cv2.dilate(th2, thresh_kernel, iterations=2)
+    thresh_kernel = np.ones((9, 9), np.uint8)
+    dilated = cv2.erode(thresh, thresh_kernel, iterations=1)
 
     (contours, _) = cv2.findContours(dilated.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -44,8 +49,10 @@ def crop_image_and_text(page):
     max_box = None
     for contour in contours:
         rect = cv2.minAreaRect(contour)
-        (_, (w, h), _) = rect
+        (_, (h, w), _) = rect
         area = w * h
+        if h > 450 or w > page.shape[1] - 25:
+            continue
         if area > max_area:
             max_area = area
             max_box = cv2.cv.BoxPoints(rect)
